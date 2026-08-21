e3ee379d7b9b29b946434c856fac447663f2b0c6
andy <andywu2014@foxmail.com>
2026-08-21 13:03:15 +0800

    求x/z值的方法变换

--- main.ts
+++ main.ts
@@ -1,20 +1,28 @@
 input.onButtonPressed(Button.A, function () {
     times += 1
-    for (let index = 0; index < 5; index++) {
-        list.unshift(input.magneticForce(Dimension.X) / input.magneticForce(Dimension.Z))
-        basic.pause(50)
-    }
-    bluetooth.uartWriteValue("x/z : " + times, (list.removeAt(0) + (list.removeAt(1) + (list.removeAt(2) + (list.removeAt(3) + list.removeAt(4))))) / 5)
-    list = []
+    a = input.magneticForce(Dimension.X) / input.magneticForce(Dimension.Z)
+    basic.pause(50)
+    b = input.magneticForce(Dimension.X) / input.magneticForce(Dimension.Z)
+    basic.pause(50)
+    c = input.magneticForce(Dimension.X) / input.magneticForce(Dimension.Z)
+    basic.pause(50)
+    d = input.magneticForce(Dimension.X) / input.magneticForce(Dimension.Z)
+    basic.pause(50)
+    e = input.magneticForce(Dimension.X) / input.magneticForce(Dimension.Z)
+    bluetooth.uartWriteValue("x/z : " + times, (a + (b + (c + (d + e)))) / 5)
 })
 input.onButtonPressed(Button.B, function () {
     times += -1
 })
-let list: number[] = []
+let e = 0
+let d = 0
+let c = 0
+let b = 0
+let a = 0
 let times = 0
 bluetooth.startUartService()
 times = 0
-list = []
+let list: number[] = []
 loops.everyInterval(1000, function () {
     bluetooth.uartWriteNumber(input.magneticForce(Dimension.X) / input.magneticForce(Dimension.Z))
 })
